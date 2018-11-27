c3fd65aa8fc4f25258e2c8fefd377807da3e51e0
AnhQuy <legiaquy@gmail.com>
2018-11-27 21:49:11 +0700

    Update server to receive and store avatar

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session'); // 1 midleware
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session"); // 1 midleware
+const cors = require("cors");
 
-
-const apiRouter = require('./routers/apiRouter');
+const apiRouter = require("./routers/apiRouter");
 
 const port = 1998;
 
@@ -15,21 +14,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(session({
-    secret: 'babyserer',
+app.use(
+  session({
+    secret: "babyserer",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, maxAge: 24*60*60*1000, httpOnly: false } //deploy-secure: true
-}))
+    cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000, httpOnly: false } //deploy-secure: true
+  })
+);
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
+app.use(express.static("./uploadFile"));
 
-mongoose.connect("mongodb://localhost/baby-service", { useNewUrlParser: true }, (err) => {
-    if(err) console.log(err)
-    else console.log('DB connect success !!');
-})
+mongoose.connect(
+  "mongodb://localhost/baby-service",
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) console.log(err);
+    else console.log("DB connect success !!");
+  }
+);
 
 app.listen(port, (err) => {
-    if(err) console.log(err);
-    else console.log(`Server is listening at ${port}`);
+  if (err) console.log(err);
+  else console.log(`Server is listening at ${port}`);
 });
