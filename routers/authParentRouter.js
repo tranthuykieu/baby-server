31d4ed4af8b8c8321fe0c52e2be905a7fba14316
AnhQuy <legiaquy@gmail.com>
2018-11-29 11:53:50 +0700

    Login & Logout

--- routers/authParentRouter.js
+++ routers/authParentRouter.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
 const authParentRouter = express.Router();
 
-const ParentModel = require('../models/parentModel');
+const ParentModel = require("../models/parentModel");
 
-authParentRouter.post('/login', (req,res) => {
-    const { phoneNumber, password } = req.body;
-    if(!phoneNumber && !password){
-        res.status(400).send({ success: 0, message: "Missing phone number or password"});
-    } else {
-        ParentModel.findOne({ phoneNumber })
-            .then(parentFound => {
-                if(!parentFound) res.status(404).send({ success: 0, message: "No parent" })
-                else {
-                    const compare = bcrypt.compareSync(password, parentFound.hashPassword);
-                    if(compare) {
-                        req.session.parent = { phoneNumber: parentFound.phoneNumber, name: parentFound.fullname, parentId: parentFound._id };
-                        res.send({ success: 0, message: "Login success"});
-                    }
-                    else res.status(401).send({ success: 0, message: "Wrong password"});
-                }
-            })
-            .catch( err => res.status(500).send({ success: 0, err }));
-    }
+authParentRouter.post("/login", (req, res) => {
+  const { phoneNumber, password } = req.body;
+  if (!phoneNumber && !password) {
+    res
+      .status(400)
+      .send({ success: 0, message: "Missing phone number or password" });
+  } else {
+    ParentModel.findOne({ phoneNumber })
+      .then((parentFound) => {
+        if (!parentFound)
+          res.status(404).send({ success: 0, message: "No parent" });
+        else {
+          const compare = bcrypt.compareSync(
+            password,
+            parentFound.hashPassword
+          );
+          if (compare) {
+            req.session.parent = {
+              phoneNumber: parentFound.phoneNumber,
+              name: parentFound.fullname,
+              parentId: parentFound._id
+            };
+            res.send({ success: 0, message: "Login success", parentId: parentFound._id });
+          } else
+            res.status(401).send({ success: 0, message: "Wrong password" });
+        }
+      })
+      .catch((err) => res.status(500).send({ success: 0, err }));
+  }
 });
 
-authParentRouter.get('/logout', (req,res) => {
-    req.session.destroy((err) => {
-        if(err) res.status(500).send({ success: 0, err })
-        else res.send({ success: 1, message: "Success !" });
-    });
+authParentRouter.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) res.status(500).send({ success: 0, err });
+    else res.send({ success: 1, message: "Success !" });
+  });
 });
-
 
 module.exports = authParentRouter;
-
--- routers/authSisterRouter.js
+++ routers/authSisterRouter.js
@@ -1,39 +1,49 @@
-const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
 const authSisterRouter = express.Router();
 
-const SisterModel = require('../models/sisterModel');
+const SisterModel = require("../models/sisterModel");
 
-authSisterRouter.post('/login', (req,res) => {
-    const { phoneNumber, password } = req.body;
-    if(!phoneNumber && !password){
-        res.status(400).send({ success: 0, message: "Missing phone number or password"});
-    } else {
-        SisterModel.findOne({ phoneNumber })
-            .then(sisterFound => {
-                if(!sisterFound) res.status(404).send({ success: 0, message: "No sister" })
-                else {
-                    const compare = bcrypt.compareSync(password, sisterFound.hashPassword);
-                    if(compare){
-                        req.session.sister = { phoneNumber: sisterFound.phoneNumber, name: sisterFound.fullname, sisterId: sisterFound._id };
-                        res.send({ success: 0, message: "Login success"})
-                    } 
-                    else res.status(401).send({ success: 0, message: "Wrong password"});
-                }
-            })
-            .catch( err => res.status(500).send({ success: 0, err }));
-    }
-
+authSisterRouter.post("/login", (req, res) => {
+  const { phoneNumber, password } = req.body;
+  if (!phoneNumber && !password) {
+    res
+      .status(400)
+      .send({ success: 0, message: "Missing phone number or password" });
+  } else {
+    SisterModel.findOne({ phoneNumber })
+      .then((sisterFound) => {
+        if (!sisterFound)
+          res.status(404).send({ success: 0, message: "No sister" });
+        else {
+          const compare = bcrypt.compareSync(
+            password,
+            sisterFound.hashPassword
+          );
+          if (compare) {
+            req.session.sister = {
+              phoneNumber: sisterFound.phoneNumber,
+              name: sisterFound.fullname,
+              sisterId: sisterFound._id
+            };
+            res.send({
+              success: 0,
+              message: "Login success",
+              sisterId: sisterFound._id
+            });
+          } else
+            res.status(401).send({ success: 0, message: "Wrong password" });
+        }
+      })
+      .catch((err) => res.status(500).send({ success: 0, err }));
+  }
 });
 
-authSisterRouter.get('/logout', (req,res) => {
-    req.session.destroy((err) => {
-        if(err) res.status(500).send({ success: 0, err })
-        else res.send({ success: 1, message: "Success !" });
-    });
+authSisterRouter.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) res.status(500).send({ success: 0, err });
+    else res.send({ success: 1, message: "Success !" });
+  });
 });
 
 module.exports = authSisterRouter;
-
-
-
